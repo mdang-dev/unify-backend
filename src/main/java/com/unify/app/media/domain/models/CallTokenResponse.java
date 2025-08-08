fbee9a212cad9ccce96961617d8102c7c3bd75fc
Minh Dang <minhdang25.dev@gmail.com>
2025-08-08 20:17:08 +0700

    chore(call): add debug on production (#29)

--- src/main/java/com/unify/app/media/domain/models/CallTokenResponse.java
+++ src/main/java/com/unify/app/media/domain/models/CallTokenResponse.java
@@ -1,11 +1,4 @@
 package com.unify.app.media.domain.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record CallTokenResponse(
-        String token,
-        boolean video,
-        boolean isCaller,
-        String calleeName,
-        String calleeAvatar) {
-}
+    String token, boolean video, boolean isCaller, String calleeName, String calleeAvatar) {}
