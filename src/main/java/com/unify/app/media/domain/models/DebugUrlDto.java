5690470df2222aa8dcbb96929132dedbfbac4691
mdang-dev <minhdang25.dev@gmail.com>
2025-08-08 20:14:18 +0700

    chore(call): add debug on production

---
+++ src/main/java/com/unify/app/media/domain/models/DebugUrlDto.java
@@ -0,0 +1,9 @@
+package com.unify.app.media.domain.models;
+
+public record DebugUrlDto(
+        String type,
+        String host,
+        String wsUrl,
+        String apiKey,
+        String apiSecret) {
+}
